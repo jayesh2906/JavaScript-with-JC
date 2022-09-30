ace6117eb9e065e653a9141327d5fbc53e2626a5
jayesh2906 <jayesh.choudhary2906@gmail.com>
2022-09-30 22:07:07 +0530

    Guess the Output?

---
+++ Guess-The-Output.js
@@ -0,0 +1,24 @@
+/* 💡"JavaScript-with-JC" - Guess the Output? */
+
+// MCQ-1
+function MCQ1() {
+  const person = {
+    name: "Jayesh",
+    displayName1: function () {
+      console.log("name1", this.name);
+    },
+    displayName2: () => {
+      console.log("name2", this);
+    },
+  };
+  person.displayName1();
+  person.displayName2();
+
+  // 👍A) name1 Jayesh , name2 Jayesh
+  // 💡B) name1 Jayesh , name2 undefined
+  // 💖C) name1 Jayesh , name2
+  // 🤔D) name1 , name2 Jayesh
+
+  /* Answer is C) name1 Jayesh , name2 because arrow function inherits "this" from its outer function where "this" is window.*/
+}
+// MCQ1();
