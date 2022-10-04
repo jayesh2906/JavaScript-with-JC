fab67b927c0b3cc7da8d62f450e1999e51b906aa
jayesh2906 <jayesh.choudhary2906@gmail.com>
2022-10-04 18:13:40 +0530

    Promise.race and Its Polyfill

--- PromiseRace-Polyfill.js
+++ PromiseRace-Polyfill.js
@@ -1,18 +1,8 @@
 /* 💡"JavaScript-with-JC"
-👉 Polyfills of Promise.all, Promise.any, and Promise.race
+👉 Promise.race() and Its Polyfill
 
-Promise.all() - It executes all passed promises parallelly and improves the performance of the application
-💡Promise.all() Cases :-
-1) If all promises resolve, returns the array of results of all promises resolved.
-2) If any promise fails, return the rejected promise error.
-3) If passed empty [], returns empty [].
+Promise.race() - It executes all passed promises concurrently and returns the first resolved or rejected promise result.
 
-Promise.any() - It executes all passed promises parallelly and returns the first resolved promise result.
-💡Promise.any() Cases :-
-1) If no promise passes, returns the AggregateError "All promises were rejected".
-2) If passed empty [], returns error.
-
-Promise.race() - It executes all passed promises parallelly and returns the first resolved or rejected promise result.
 💡Promise.race() Case :-
 1) If passed empty [], forever pending.
 */
