46e26c1392ba9c6c9d12c71000faebb1c6cb1f96
jayesh2906 <jayesh.choudhary2906@gmail.com>
2022-10-08 21:44:37 +0530

    Promise Polyfill

--- Promises-Polyfill.js
+++ Promises-Polyfill.js
@@ -235,4 +235,81 @@ function promisePolyfillCode() {
     console.log(error); // problem in generating number!
   });
 }
-promisePolyfillCode();
+// promisePolyfillCode();
+
+function promisePolyfillCode2() {
+  function CustomPromise(executer) {
+    let OnResolve,
+      OnReject,
+      isFulfilled = false,
+      isRejected = false,
+      isCalled = false,
+      value;
+
+    function resolve(val) {
+      console.log("in resolve");
+      isFulfilled = true;
+      value = val;
+      if (typeof OnResolve === "function") {
+        OnResolve(value);
+        isCalled = true;
+      }
+    }
+
+    function reject(val) {
+      console.log("in reject");
+      isRejected = true;
+      value = val;
+      if (typeof OnReject === "function") {
+        OnReject(value);
+        isCalled = true;
+      }
+    }
+
+    this.then = function (callback) {
+      console.log("in then");
+      OnResolve = callback;
+
+      if (isFulfilled && !isCalled) {
+        isCalled = true;
+        OnResolve(value);
+      }
+      return this;
+    };
+
+    this.catch = function (callback) {
+      console.log("in catch");
+      OnReject = callback;
+
+      if (isRejected && !isCalled) {
+        isCalled = true;
+        OnReject(value);
+      }
+      return this;
+    };
+
+    try {
+      executer(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
+  }
+
+  const executor = (resolve, reject) => {
+    console.log("generating number...");
+    setTimeout(() => {
+      resolve(2);
+    }, 1000);
+  };
+
+  const generateNumber = new CustomPromise(executor);
+
+  generateNumber
+    .then((result) => {
+      console.log(result); // number generated successfully 6.
+    })
+    .catch((error) => {
+      console.log(error); // problem in generating number!
+    });
+}
+promisePolyfillCode2();
